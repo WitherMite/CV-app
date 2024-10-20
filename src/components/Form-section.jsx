2144e06b2a5179bf683eaa0f3867d405d515ac99
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-10-19 21:32:20 -0500

    Generate input fields

--- src/components/Form-section.jsx
+++ src/components/Form-section.jsx
@@ -3,24 +3,20 @@ import InputField from "./Input-field.jsx";
 // takes in an array representing questions to create a fieldset
 // has a flag prop to allow generating a repeated subsection with a button (for edu/work exp)
 // if submitted, fieldset is hidden, disabled, and replaced with an element containing submitted info
-export default function FormSection({ title, isSubmitted }) {
+export default function FormSection({ fields, title, isSubmitted }) {
   return (
     <fieldset disabled={isSubmitted}>
       <legend>{title}</legend>
-      <InputField
-        labelText="This is a test"
-        inputName="1"
-        inputId="test"
-        inputType="text"
-        inputValue="yep"
-      ></InputField>
-      <InputField
-        labelText="This is a test2"
-        inputName="2"
-        inputId="test2"
-        inputType="textarea"
-        inputValue="yep"
-      ></InputField>
+      {fields.map((field) => (
+        <InputField
+          key={field.id}
+          labelText={field.label}
+          inputName={field.name}
+          inputId={field.inputId}
+          inputType={field.type}
+          inputValue={field.defaultValue}
+        ></InputField>
+      ))}
     </fieldset>
   );
 }
